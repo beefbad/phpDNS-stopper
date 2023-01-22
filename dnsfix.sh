20d484be71b11f537e1e959d691e2561d8d9d93b
beefbad <beefbad@proton.me>
2023-01-22 07:44:59 -0800

    update

--- dnsfix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$(ps ax | grep "php /Users/apple/.phpDNS/dnsfix.php" | grep -vc grep)" -lt 1 ]; then
-	php /Users/apple/.phpDNS/dnsfix.php
-else
-	printf "phpDNS is already running!\n"
-fi
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod +x dnsfix.php
-chmod +x dnsfix.sh
-sudo cp -R com.beefbad.phpDNS.plist /Library/LaunchDaemons/com.beefbad.phpDNS.plist
-sudo chown root:wheel /Library/LaunchDaemons/com.beefbad.phpDNS.plist
-sudo launchctl load -w /Library/LaunchDaemons/com.beefbad.phpDNS.plist
