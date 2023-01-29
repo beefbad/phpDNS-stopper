19a887d3ce6f86b4545be44f1eb66dff28adbe30
beefbad <beefbad@proton.me>
2023-01-29 04:02:55 -0800

    fix

--- one-click-install.sh
+++ one-click-install.sh
@@ -9,7 +9,7 @@ fi
 
 echo -n -e "#!/bin/bash\nif [ \"\$(ps ax | grep \"php $DIR/phpDNSfix.php\" | grep -vc grep)\" -lt 1 ]; then\n\tphp $DIR/phpDNSfix.php\nelse\n\tprintf \"phpDNSfix.php is already running!\"\nfi\n">$DIR/phpDNSfix.sh
 echo -n -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n<dict>\n\t<key>Label</key>\n\t<string>com.beefbad.phpDNS</string>\n\t<key>KeepAlive</key>\n\t<true/>\n\t<key>RunAtLoad</key>\n\t<true/>\n\t<key>WorkingDirectory</key>\n\t<string>/$DIR</string>\n\t<key>ProgramArguments</key>\n\t<array>\n\t\t<string>$DIR/phpDNSfix.sh</string>\n\t</array>\n\t<key>ThrottleInterval</key>\n\t<integer>1</integer>\n\t<key>Nice</key>\n\t<integer>1</integer>\n\t<key>UserName</key>\n\t<string>root</string>\n\t<key>StandardErrorPath</key>\n\t<string>$DIR/Logs/phpDNSstopper.log</string>\n\t<key>StandardOutPath</key>\n\t<string>$DIR/Logs/phpDNSstopper.log</string>\n</dict>\n</plist>">$LaunchDaemonPath
-echo -n -e "#!/bin/bash\nsudo launchctl unload -w /Library/LaunchDaemons/com.beefbad.phpDNS.plist\nsudo launchctl remove -w /Library/LaunchDaemons/com.beefbad.phpDNS.plist\nsudo rm -rfv /Library/LaunchDaemons/com.beefbad.phpDNS.plist">uninstall.sh
+echo -n -e "#!/bin/bash\nsudo launchctl unload -w $LaunchDaemonPath\nsudo launchctl remove -w $LaunchDaemonPath\nsudo rm -rfv $LaunchDaemonPath">uninstall.sh
 
 mkdir $DIR/Logs
 chmod +w $DIR/Logs
@@ -18,8 +18,8 @@ sudo chown root:wheel $DIR/dphpDNSfix.php
 sudo chown root:wheel $DIR/phpDNSfix.sh
 sudo chmod +x $DIR/phpDNSfix.php
 sudo chmod +x $DIR/phpDNSfix.sh
-sudo chown root:wheel /Library/LaunchDaemons/com.beefbad.phpDNS.plist
-sudo launchctl load -w /Library/LaunchDaemons/com.beefbad.phpDNS.plist
+sudo chown root:wheel $LaunchDaemonPath
+sudo launchctl load -w /$LaunchDaemonPath
 while read -r line; do
 	sname=$(echo "$line" | awk -F  "(, )|(: )|[)]" '{print $2}')
 	sdev=$(echo "$line" | awk -F  "(, )|(: )|[)]" '{print $4}')
